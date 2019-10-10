63d349a64c0c29d8104ae929b06984d0eaedd7d1
Yini <ydxb7@mail.missouri.edu>
2019-10-10 02:36:59 -0700

    add select wallet fragment add navigate to it

--- app/src/main/java/ai/tomorrow/tokenjar/DefaultWalletFragment.kt
+++ app/src/main/java/ai/tomorrow/tokenjar/DefaultWalletFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 
 class DefaultWalletFragment : Fragment(){
 
@@ -14,6 +16,12 @@ class DefaultWalletFragment : Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         binding = FragmentDefaultWalletBinding.inflate(inflater, container, false)
+
+        binding.selectWalletBtn.setOnClickListener{
+            val direction = HomeViewPagerFragmentDirections.actionHomeViewPagerFragment2ToSelectWalletFragment()
+            it.findNavController().navigate(direction)
+        }
+
 
 
         return binding.root
---
+++ app/src/main/java/ai/tomorrow/tokenjar/adapters/SelectWalletFragment.kt
@@ -0,0 +1,30 @@
+package ai.tomorrow.tokenjar.adapters
+
+import ai.tomorrow.tokenjar.databinding.FragmentSelectWalletBinding
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+
+class SelectWalletFragment : Fragment(){
+
+    private lateinit var binding: FragmentSelectWalletBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSelectWalletBinding.inflate(inflater, container, false)
+
+        binding.cancelBtn.setOnClickListener{
+            it.findNavController().navigateUp()
+        }
+
+        return binding.root
+    }
+
+
+}
