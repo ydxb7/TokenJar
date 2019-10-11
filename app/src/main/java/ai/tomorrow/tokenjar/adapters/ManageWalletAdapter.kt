a5df751d3d86a858f243d8fb0c2757bd46fc11da
Yini <ydxb7@mail.missouri.edu>
2019-10-10 18:28:21 -0700

    click wallet to navigate to manage

--- app/src/main/java/ai/tomorrow/tokenjar/adapters/ManageWalletAdapter.kt
+++ app/src/main/java/ai/tomorrow/tokenjar/adapters/ManageWalletAdapter.kt
@@ -3,6 +3,7 @@ package ai.tomorrow.tokenjar.adapters
 import ai.tomorrow.tokenjar.HomeViewPagerFragmentDirections
 import ai.tomorrow.tokenjar.data.EthWallet
 import ai.tomorrow.tokenjar.databinding.ListItemManageWalletBinding
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,6 +14,9 @@ import androidx.recyclerview.widget.RecyclerView
 
 class ManageWalletAdapter :
     ListAdapter<EthWallet, ManageWalletAdapter.ViewHolder>(WalletDiffCallback()) {
+
+    public val TAG = "ManageWalletAdapter"
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(ListItemManageWalletBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
@@ -28,6 +32,7 @@ class ManageWalletAdapter :
         init {
             // TODO navigate to ManageFragment
             binding.setClickListener { view ->
+                Log.d("ManageWalletAdapter", "click to navigate to manage")
                 binding.wallet?.let { wallet ->
                     navigateToManage(wallet, view)
                 }
