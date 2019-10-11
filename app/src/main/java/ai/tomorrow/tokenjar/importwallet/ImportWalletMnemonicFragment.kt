6b7e21d74ef5bc79303fe8c07a0ad0df269ab246
Yini <ydxb7@mail.missouri.edu>
2019-10-10 20:41:39 -0700

    can install

--- app/src/main/java/ai/tomorrow/tokenjar/importwallet/ImportWalletMnemonicFragment.kt
+++ app/src/main/java/ai/tomorrow/tokenjar/importwallet/ImportWalletMnemonicFragment.kt
@@ -11,14 +11,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import org.consenlabs.tokencore.wallet.Identity
+import org.consenlabs.tokencore.wallet.KeystoreStorage
+import org.consenlabs.tokencore.wallet.WalletManager
+import org.consenlabs.tokencore.wallet.model.Network
+import org.consenlabs.tokencore.wallet.model.Metadata
 import java.io.File
 
-class ImportWalletMnemonicFragment : Fragment() {
+class ImportWalletMnemonicFragment : Fragment() , KeystoreStorage {
 
     private val TAG = "ImportWalletMnemonic"
 
     private lateinit var binding: FragmentImportWalletMnemonicBinding
-//    private lateinit var identity: Identity
+    private lateinit var identity: Identity
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,23 +32,23 @@ class ImportWalletMnemonicFragment : Fragment() {
     ): View? {
         binding = FragmentImportWalletMnemonicBinding.inflate(inflater, container, false)
 
-//        WalletManager.storage = this
-//        WalletManager.scanWallets()
+        WalletManager.storage = this
+        WalletManager.scanWallets()
 
         binding.importWalletBtn.setOnClickListener {
-//            if (binding.passwordHintEt.text.isEmpty() || binding.repeatPasswordEt.text.isEmpty()) {
-//                Toast.makeText(context, "Password is required.", Toast.LENGTH_SHORT).show()
-//            } else if (binding.passwordHintEt.text != binding.repeatPasswordEt.text) {
-//                Toast.makeText(context, "Passwords do not match.", Toast.LENGTH_SHORT).show()
-//            } else if (binding.mnemonicEv.text.isEmpty()) {
-//                Toast.makeText(context, "Mnemonic phrases is required.", Toast.LENGTH_SHORT).show()
-//            } else {
-//                Log.d(TAG, "create wallet.")
-//                val wallet = getWalletFromMnemonic(binding.mnemonicEv.text.toString())
-//                val walletRepository =
-//                    WalletRepository.getInstance(WalletDatabase.getInstance(requireContext()).walletDatabaseDao)
-//                walletRepository.insertWallet(wallet)
-//            }
+            if (binding.passwordHintEt.text.isEmpty() || binding.repeatPasswordEt.text.isEmpty()) {
+                Toast.makeText(context, "Password is required.", Toast.LENGTH_SHORT).show()
+            } else if (binding.passwordHintEt.text != binding.repeatPasswordEt.text) {
+                Toast.makeText(context, "Passwords do not match.", Toast.LENGTH_SHORT).show()
+            } else if (binding.mnemonicEv.text.isEmpty()) {
+                Toast.makeText(context, "Mnemonic phrases is required.", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.d(TAG, "create wallet.")
+                val wallet = getWalletFromMnemonic(binding.mnemonicEv.text.toString())
+                val walletRepository =
+                    WalletRepository.getInstance(WalletDatabase.getInstance(requireContext()).walletDatabaseDao)
+                walletRepository.insertWallet(wallet)
+            }
 
 
         }
@@ -54,36 +59,36 @@ class ImportWalletMnemonicFragment : Fragment() {
         return binding.root
     }
 
-//    private fun getWalletFromMnemonic(mnemonic: String): EthWallet {
-//        val password = binding.walletPasswordEt.text.toString()
-//        val passwordHint = binding.passwordHintEt.text.toString()
-//
-//        identity = Identity.recoverIdentity(
-//            binding.mnemonicEv.text.toString(),
-//            "identity1",
-//            password,
-//            passwordHint,
-//            Network.ROPSTEN,
-//            Metadata.NONE
-//        )
-//        Log.d(TAG, "get identity")
-//
-//        val tokenCoreWallet = identity.wallets[0]
-//        val wallet = EthWallet()
-//        wallet.address = "0x${tokenCoreWallet.address}"
-//        wallet.keystore = WalletManager.exportKeystore(tokenCoreWallet.id, password)
-//        wallet.keystorePath =
-//            requireActivity().filesDir.absolutePath + "/wallets" + "/${tokenCoreWallet.id}.json"
-//        wallet.privateKey = WalletManager.exportPrivateKey(tokenCoreWallet.id, password)
-//        wallet.mnemonic = WalletManager.exportMnemonic(tokenCoreWallet.id, "").mnemonic
-//        wallet.name = "new wallet"
-//
-//        return wallet
-//    }
+    private fun getWalletFromMnemonic(mnemonic: String): EthWallet {
+        val password = binding.walletPasswordEt.text.toString()
+        val passwordHint = binding.passwordHintEt.text.toString()
 
-//    override fun getKeystoreDir(): File {
-////        Log.d(TAG, "getKeystoreDir = $filesDir")
-//        return requireActivity().filesDir
-//
-//    }
+        identity = Identity.recoverIdentity(
+            binding.mnemonicEv.text.toString(),
+            "identity1",
+            password,
+            passwordHint,
+            Network.ROPSTEN,
+            Metadata.NONE
+        )
+        Log.d(TAG, "get identity")
+
+        val tokenCoreWallet = identity.wallets[0]
+        val wallet = EthWallet()
+        wallet.address = "0x${tokenCoreWallet.address}"
+        wallet.keystore = WalletManager.exportKeystore(tokenCoreWallet.id, password)
+        wallet.keystorePath =
+            requireActivity().filesDir.absolutePath + "/wallets" + "/${tokenCoreWallet.id}.json"
+        wallet.privateKey = WalletManager.exportPrivateKey(tokenCoreWallet.id, password)
+        wallet.mnemonic = WalletManager.exportMnemonic(tokenCoreWallet.id, "").mnemonic
+        wallet.name = "new wallet"
+
+        return wallet
+    }
+
+    override fun getKeystoreDir(): File {
+//        Log.d(TAG, "getKeystoreDir = $filesDir")
+        return requireActivity().filesDir
+
+    }
 }
